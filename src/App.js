40d01ee6edba521f4e89dddb263144d2187bd514
rYogeshwaran369 <ryogeshwaran369@gmail.com>
2023-07-24 21:29:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Pages/Login';
+// import Login from './Pages/Login';
 import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 import Home from './Insurence/Pages/Home';
-
+import Topnav from './Insurence/Components/Topnav';
+import Explore from './Insurence/Pages/Explore';
+import Renew from './Insurence/Pages/Renew';
+import Claim from './Insurence/Pages/Claim';
+import Help from './Insurence/Pages/Help';
+import Learnmore from './Insurence/Pages/learnmore';
+import Store from './Insurence/Pages/Store';
+import { Provider } from 'react-redux';
+import Logincmp from './Insurence/Pages/Login';
+import Register from './Insurence/Pages/Register';
+import Newuser from './Insurence/Pages/newuser';
 
 function App() {
   return (
+    <Provider store={Store}>
     <Router>
-      {/* <Navbar/> */}
-      {/* <Topnav/> */}
+      <Topnav/>
       <Routes>
-        {/* <Route exact path="/" element={<Login/>} />
-        <Route path="/register" element={<Register/>} /> */}
+      <Route path='/' element={<Logincmp/>}/>
+        <Route path='/new' element={<Newuser/>}/>
         <Route path="/home" element={<Home/>}/>
-        {/* <Route path="/explore" element={<Explore/>}/> */}
+        <Route path='/explore' element={<Explore/>}/>
+        <Route path='/renew' element={<Renew/>}/>
+        <Route path='/claim' element={<Claim/>}/>
+        <Route path='/help' element={<Help/>}/>
+        <Route path='/learnmore' element={<Learnmore/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
     </Router>
+    </Provider>
   );
 }
 
